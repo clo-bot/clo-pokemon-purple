b7b875edd8bfb56c79fb77a186110394fd8ab019
Carolyn Lo <carminelle@gmail.com>
2020-03-29 13:14:33 -0400

    game update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function () {
+  disableStartBtn();
   let randomPokemon;
-  $(".choose").text("Choose Your Pokemon!")
+  let yourPokemon;
+  let yourPokemonMovesArray = []
+  $(".message").text("Choose Your Pokemon!")
+  
   async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
       await callback(array[index], index, array);
@@ -31,7 +35,7 @@ $(function () {
   $('.pokemon-selector-grid').click(event => {
     const clicked = event.target.closest('.grid-pokemon-display')
     getSpecificPokemon(clicked.id)
-    $(".choose").text("")
+    $(".message").text("")
 
   })
   async function getSpecificPokemon(pokemon) {
@@ -39,20 +43,23 @@ $(function () {
     console.log(response.data);
     displayYourPokemon(response.data)
     displayYourPokemonMoves(response.data)
+    enableStartBtn();
   }
   $(".select-button").click((event) => {
-    event.preventDefault()
-    console.log("submitting form")
+    console.log("selecting pokemon")
     getAllPokemon();
-    disableBtn();
+    disableSelectBtn();
 
   })
   function displayYourPokemon(pokemon) {
     $('.pokemon-selector-grid').html("")
+    $(".message").html(`<h1> You picked <font color="#0000e5">${pokemon.species.name}
+    </font>!</h1>`)
     $("#your-pokemon").html(`
-    <h1> You picked <font color="#0000e5">${pokemon.species.name}</font>!</h1>
     <div class="your-pokemon-display">
-      <center><img src="${pokemon.sprites.front_default}"></center></div>`)
+      <center><img src="${pokemon.sprites.front_default}"></center>
+      <p><h2>Choose 4 Moves for Your Pokemon!</h2></p></div>`)
+
   }
 
   function displayYourPokemonMoves(pokemon) {
@@ -63,19 +70,84 @@ $(function () {
       return move.move.name
     })
     moves.forEach((move) => {
-    $('.pokemon-moves-selector-grid').append(
-      `<br><div class="grid-your-pokemon-moves" id="${move}"><p>${move}</p>
-      </div>
-      `)
+      $('.pokemon-moves-selector-grid').append(
+        `<div class="grid-your-pokemon-moves" id="${move}"><p>${move}</p>
+        </div>
+        `)
     })
   }
-  // <button id="start">Start!</button>
 
+  $('body').on('click', '.grid-your-pokemon-moves', (event) => {
+    // store the id of the selected move in a variable
+    const selectedMove = $(event.currentTarget).attr('id')
+    console.log(`move selected: ${selectedMove}`)
+    yourPokemonMovesArray.push(`${selectedMove}`)
+    console.log(yourPokemonMovesArray)
+
+    // toggleClass that changes the background color of the selected move
+    $(event.currentTarget).toggleClass('selected-move')
+     if(yourPokemonMovesArray.length === 4){
+        // return yourPokemonMovesArray.filter((move, index) => 
+        // move.indexOf(move) !== index);
+        $('body').unbind();
+       }
+      console.log(yourPokemonMovesArray)
+    })
+ 
+  $(".start-button").click((event) => {
+      console.log("BEGIN BATTLE!")
+      startBattleScreen();
+  
+    })
   async function generatePokemonArray(pokemon) {
-    const randomPokemon = pokemon[Math.floor(Math.random() * 151)]
+    let randomPokemon = pokemon[Math.floor(Math.random() * 151)]
     console.log(randomPokemon)
   }
-  function disableBtn() {
-    $(".select-button").toggle()
+
+  function startBattleScreen() {
+    disableStartBtn();
+    $('.grid-pokemon-display').html("");
+    $(".pokemon-moves-selector-grid").html("");
+    $("#your-Pokemon").html("");
+    console.log(yourPokemon)
+    console.log(randomPokemon)
+    console.log(yourPokemonMovesArray)
+    $(".message").html(`${randomPokemon} vs. ${yourPokemon}`); 
+    moveButtons();
+
   }
+
+  function moveButtons() {
+    enableMoveBtn();
+    $('.move-1').text(yourPokemonMovesArray[0]);
+    $('.move-2').text(yourPokemonMovesArray[1]);
+    $('.move-3').text(yourPokemonMovesArray[2]);
+    $('.move-4').text(yourPokemonMovesArray[3]);
+
+    
+  }
+
+  function disableSelectBtn() {
+    $(".select-button").toggle()
+
+  }
+  
+  function enableStartBtn() {
+    $(".start-button").toggleClass("disabled");    
+
+  }
+
+  function disableStartBtn() {
+    $(".start-button").toggle();
+  }
+
+  function enableMoveBtn() {
+    $(".move-1").toggleClass("disabled");    
+    $(".move-2").toggleClass("disabled");    
+    $(".move-3").toggleClass("disabled");    
+    $(".move-4").toggleClass("disabled");    
+
+
+  }
+
 })
