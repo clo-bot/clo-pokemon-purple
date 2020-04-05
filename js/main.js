c67a650963386d886a5a0ec4b3b5d0ca9d67590d
Carolyn Lo <carminelle@gmail.com>
2020-04-05 17:45:18 -0400

    final finished pokemon game (๑⚈ ․̫ ⚈)

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ $(function () {
   disableStartBtn();
   disableNextBtn();
   let yourPokemon;
-  let yourPokemonMovesArray = []
+  let yourPokemonMovesArray = [];
   let yourPokemonMoves;
   let randomPokemonData;
   let yourPokemonHP;
   let randomPokemonHP;
+  let randomPokemonAllMoves = [];
+  let randomPokemonMoveName;
   let randomPokemonMove;
   let yourPokemonMovePower;
   let yourAttack;
@@ -14,6 +16,8 @@ $(function () {
   let randomPokemonAttack;
   let randomPokemonDefense;
   $(".select-message").text("Choose Your Pokemon!")
+  $(".main-image").html(`<img id="main-pic" src="images/pokeball.gif">`)
+  $(".win-container").toggleClass("disabled", true)
   
   async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
@@ -42,6 +46,8 @@ $(function () {
   $(".select-button").click((event) => {
     // console.log("selecting pokemon")
     $(".select-message").text("")
+    $(".move-message").toggleClass("disabled", true)
+    $(".main-image").html("")
     getAllPokemon();
     disableSelectBtn();
   })
@@ -53,14 +59,14 @@ $(function () {
     )
   }
 
-    $('.pokemon-selector-grid').click(event => {
-      if ($(".pokemon-selector-grid").children().length != 151) {
-        window.alert("Please wait for all Pokemon to load before selecting.")
-      } else {
-        const clicked = event.target.closest('.grid-pokemon-display')
-        getSpecificPokemon(clicked.id)
-        }  
-    })
+  $('.pokemon-selector-grid').click(event => {
+    if ($(".pokemon-selector-grid").children().length != 151) {
+      window.alert("Please wait for all Pokemon to load before selecting.")
+    } else {
+      const clicked = event.target.closest('.grid-pokemon-display')
+      getSpecificPokemon(clicked.id)
+      }  
+  })
 
   async function getSpecificPokemon(pokemon) {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -74,10 +80,9 @@ $(function () {
   function displayYourPokemon(pokemon) {
     scrollUp();
     $('.pokemon-selector-grid').html("")
-    $("#your-pokemon").html(`<div class="your-pokemon-display">
-      <h1>You picked <font color="#0000e5">${pokemon.species.name}</font>!</h1>
+    $("#your-pokemon").html(`<h1>You picked <font color="#0000e5">${pokemon.species.name}</font>!</h1>
       <p><center><img src="${pokemon.sprites.front_default}"></center></p>
-      <p><h2>Choose 4 Moves for Your Pokemon!</h2></p></div>`)
+      <p><h2>Choose 4 Moves for Your Pokemon!</h2></p>`)
   }
 
   function displayYourPokemonMoves(pokemon) {
@@ -159,7 +164,8 @@ $(function () {
     console.log(randomPokemon)
     try {
       const response = await axios.get(`${randomPokemon.url}`)
-      generateRandomPokemonMove(response.data.moves)
+      randomPokemonAllMoves = response.data.moves
+        // console.log(`This is all random's moves: ${randomPokemonAllMoves}`)
       assignPokemonStats(response.data.stats, "random")
       randomPokemonData = response.data
     } catch (error) {
@@ -194,27 +200,38 @@ $(function () {
           break;
       }
     })
-      console.log(`This is your HP: ${yourPokemonHP}. This is your attack: ${yourAttack}.
-        This is your defense: ${yourDefense}. This is random HP: ${randomPokemonHP}. 
-        This is random attack: ${randomPokemonAttack}.
-        This is random defense: ${randomPokemonDefense}.`)    
+      // console.log(`This is your HP: ${yourPokemonHP}. This is your attack: ${yourAttack}.
+      //   This is your defense: ${yourDefense}. This is random HP: ${randomPokemonHP}. 
+      //   This is random attack: ${randomPokemonAttack}.
+      //   This is random defense: ${randomPokemonDefense}.`)    
   }
 
   function startBattleScreen() {
     disableStartBtn();
     $('.grid-pokemon-display').html("");
     $(".pokemon-moves-selector-grid").html("");
-    $(".your-pokemon-display").html("");
-    $(".pokemon-battle-screen").html(`<div class="pokemon-battle-screen">
-      <center><img src="${yourPokemon.sprites.front_default}" width="200">
-      <img src="${randomPokemonData.sprites.front_default}" witdh="200">
-      </center></div>`);
+    $("#your-pokemon").html("");
+    $(".pokemon-arena").toggleClass("disabled", false);
+    $(".opponent-arena-container").html(`
+      <div class="opponent-stats">
+      <h3>${randomPokemon.name}</h3>
+      <span class="opponent-hp">HP: ${randomPokemonHP}</span></div>
+      <div class="opponent-pokemon-div">
+      <img src="${randomPokemonData.sprites.front_default}" class="opponent-sprite">
+      </div>`)
+    $(".your-arena-container").html(`
+      <div class="your-pokemon-div">
+        <img src="${yourPokemon.sprites.back_default}" class="your-sprite"/>
+      </div>
+      <div class="your-stats">
+        <h3>${yourPokemon.name}</h3>
+        <span class="your-hp">HP: ${yourPokemonHP}</span>
+      </div>`
+     );
+    $("#your-turn-message").html(`${yourPokemon.name}'s <br>turn!`)
     $(".versus").html(`<font color="#0000e5">${yourPokemon.name}</font> vs. ${randomPokemon.name}!`);
 
     printHP();
-
-    $(".pokemon-order").html(`<p><font color="#0000e5">${yourPokemon.name}</font> go first!`); 
-    
     moveButtons();    
     yourMoves();
   }
@@ -238,22 +255,26 @@ $(function () {
       try {
         const response = await axios.get(`${yourPokemonMove.url}`)
         yourPokemonMovePower = response.data.power
-        console.log(randomPokemonHP)
-        console.log(yourPokemonMovePower)
+        // console.log(randomPokemonHP)
+        // console.log(yourPokemonMovePower)
         const damageDone = attackDamage(yourAttack, yourPokemonMovePower, randomPokemonDefense)
           console.log(`This is damage done: ${damageDone}`)
         randomPokemonHP = randomPokemonHP - damageDone
+          $(".opponent-sprite").toggleClass("shake")
 
-          $(".pokemon-order").html(""); 
-
+          $(".move-message").toggleClass("disabled", false)
           $(".move-message").text(`You picked ${yourPokemonMove.name} and caused ${damageDone} 
           damage!`);
-      
+
+          scrollUp();
           printHP();
           disableMoveBtn();
           enableNextBtn();
           checkGame();
 
+          setTimeout(() => {
+            $(".opponent-sprite").toggleClass("shake", false)
+          }, 1000)
           // console.log(`This is random HP: ${randomPokemonHP}`)
         } catch (error) {
           console.log(error)
@@ -264,38 +285,55 @@ $(function () {
   }
 
   $(".next-button").click((event) => {
+    scrollUp();
+    generateRandomPokemonMove();
     randomPokemonMoves();
   })
 
 
-  function generateRandomPokemonMove (moves) {
-    randomPokemonMove = moves[Math.floor(Math.random() * moves.length)]
-    console.log(randomPokemonMove)
+  function generateRandomPokemonMove() {
+    let moves = randomPokemonAllMoves
+    randomPokemonMoveName = moves[Math.floor(Math.random() * moves.length)]
+    console.log(`This is random pokemon move name ${randomPokemonMoveName}`)
   }
 
   async function randomPokemonMoves() {
       try {
         disableNextBtn();
-        console.log(randomPokemonMove)
+        // console.log(`This is All Moves: ${randomPokemonAllMoves}`)
+        randomPokemonMove = randomPokemonAllMoves.find((randomPokemonOneMove) => {
+          return randomPokemonOneMove.move.name === randomPokemonMoveName.move.name
+          // console.log(`This is random pokemone one move ${randomPokemonOneMove.move.name}`)
+          // console.log(`This is random pokemone move name ${randomPokemonMoveName.move.name}`)
+          // console.log(randomPokemonMove)
+        })
+        console.log(`This is Random Move: ${randomPokemonMove.move.name}`)
         const response = await axios.get(`${randomPokemonMove.move.url}`)
         randomPokemonMovePower = response.data.power
         // console.log(`This is your HP: ${yourPokemonHP}`)
-        // console.log(`This is Random Pokemon's Move Power: ${randomPokemonMovePower}`)
+        console.log(`This is Random Pokemon's Move Power: ${randomPokemonMovePower}`)
         const damageDone = attackDamage(randomPokemonAttack, randomPokemonMovePower, yourDefense)
           console.log(`This is damage done: ${damageDone}`)
         yourPokemonHP = yourPokemonHP - damageDone
 
+        $(".move-message").toggleClass("disabled", false)
+        $(".move-message").text(`${randomPokemon.name} picked ${randomPokemonMove.move.name} 
+          and caused ${damageDone} damage!`);
+
+        $(".your-sprite").toggleClass("shake")
+      
           printHP();
           checkGame();
 
-          $(".move-message").text(`${randomPokemon.name} picked ${randomPokemonMove.move.name} 
-          and caused ${damageDone} damage!`);
-
-          $(".pokemon-order").html(`<p><font color="#0000e5">${yourPokemon.name}</font>'s turn!`); 
+          $("#your-turn-message").html(`${yourPokemon.name}'s <br>turn!`); 
 
           enableMoveBtn();
 
           console.log(`This is your HP: ${yourPokemonHP}`)
+          setTimeout(() => {
+            $(".opponent-sprite").toggleClass("shake", false)
+          }, 1000)
+
         } catch (error) {
           console.log(error)
           }
@@ -322,22 +360,32 @@ $(function () {
 
   function checkGame() {
     if (randomPokemonHP <= 0) {
+      $(".win-container").toggleClass("disabled", false)
       $(".win-message").html(
       `<center>
       ${randomPokemon.name} is defeated!
       <br>You are the Pokemon master!
       </center>`)
-      $(".pokemon-order").text("")
+      $(".win-image").html(`<img id="squirtle" src="images/squirtle.gif">`)
+      $("#your-turn-message").text("")
+      $(".pokemon-arena").toggleClass("disabled");
+      disableMoveBtn();
       disableNextBtn();
+      endGame();
       console.log("You win")
     } else if (yourPokemonHP <= 0){
+      $(".win-container").toggleClass("disabled", false)
       $(".win-message").html(
       `<center>
       ${yourPokemon.name} is defeated!
       <br>You need to train more!
       </center>`)
-      $(".pokemon-order").text("")
+      $(".win-image").html(`<img id="sadpika" src="images/sadpika.gif">`)
+      $("#your-turn-message").text("")
+      $(".pokemon-arena").toggleClass("disabled");
+      disableMoveBtn();
       disableNextBtn();
+      endGame();
       console.log("random win")
     } else {
       console.log("keep going")
@@ -345,40 +393,57 @@ $(function () {
   }
 
   function printHP() {
-    $(".your-pokemon-hp-display").text(`${yourPokemon.name} HP = ${yourPokemonHP}`);
-    $(".random-pokemon-hp-display").text(`${randomPokemon.name} HP = ${randomPokemonHP}`);
+    $(".your-hp").text(`HP = ${yourPokemonHP}`);
+    $(".opponent-hp").text(`HP = ${randomPokemonHP}`);
   }
 
   function disableSelectBtn() {
-    $(".select-button").toggle()
-
+    $(".select-button").toggleClass("disabled", true);
   }
+
+  function endGame() {
+    $(".start-over-button").toggleClass("disabled", false);  
+    yourPokemon = null;
+    yourPokemonMovesArray = []
+    yourPokemonMoves = null;
+    randomPokemonData = null;
+    yourPokemonHP = null;
+    randomPokemonHP = null;
+    randomPokemonMove = null;
+    yourPokemonMovePower = null;
+    yourAttack = null;
+    yourDefense = null;
+    randomPokemonAttack = null;
+    randomPokemonDefense = null;
+  }
+
+  $(".start-over-button").click(()=> {
+    startOverBtn()
+  })
   
   function enableStartBtn() {
     $(".start-button").toggleClass("disabled");    
-    console.log("enabling start button")
+    // console.log("enabling start button")
   }
 
   function disableStartBtn() {
     $(".start-button").toggleClass("disabled", true);
   }
 
+  function startOverBtn() {
+    location.reload();
+  }
+
   function enableMoveBtn() {
-    $(".move-1").toggleClass("disabled");    
-    $(".move-2").toggleClass("disabled");    
-    $(".move-3").toggleClass("disabled");    
-    $(".move-4").toggleClass("disabled");    
+    $(".pokemon-battle-screen").toggleClass("disabled", false);  
   }
 
   function disableMoveBtn() {
-    $(".move-1").toggleClass("disabled", true);    
-    $(".move-2").toggleClass("disabled", true);    
-    $(".move-3").toggleClass("disabled", true);    
-    $(".move-4").toggleClass("disabled", true);    
+    $(".pokemon-battle-screen").toggleClass("disabled", true);  
   }
 
   function enableNextBtn() {
-    $(".next-button").toggleClass("disabled");    
+    $(".next-button").toggleClass("disabled", false);    
   }
 
   function disableNextBtn() {
